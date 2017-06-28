7b952b8d9b00945cd87d1d6779b234ad68974073
arsenalup <676250063@qq.com>
2017-06-29 00:31:27 +0800

    6.29

---
+++ 丑数.py
@@ -0,0 +1,27 @@
+class Solution:
+    """
+    @param {int} n an integer.
+    @return {int} the nth prime number as description.
+    """
+    def nthUglyNumber(self, n):
+        # write your code here
+        import heapq
+        if n <=1:
+            return n
+            
+        n -=1
+        key=[2,3,5]
+        h=[]
+        for i in range(3):
+            heapq.heappush(h, (key[i], i))
+        
+        value = key[0]
+        while n>0:
+            value, level = heapq.heappop(h)
+            while level <3:
+                new_value = key[level] * value
+                heapq.heappush(h, (new_value, level))
+                level +=1
+            n -=1
+        return value
+        
