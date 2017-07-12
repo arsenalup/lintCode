195207f2c775c7c62b4456910bc1f8096bd09d32
arsenalup <676250063@qq.com>
2017-07-12 23:45:55 +0800

    7.12

---
+++ 二分查找.py
@@ -0,0 +1,17 @@
+def search(i, j):
+    low, high = 0, len(i)-1
+    while low < high:
+        print(low, high)
+        mid = int((low + high)/2 +1)
+        if i[mid] > j:
+            high = mid
+        elif i[mid] <j:
+            low = mid +1
+        else:
+            return mid
+
+    return low if i[low] == j else False
+
+if __name__ == '__main__':
+    i = [1, 4, 12, 45, 66, 99, 120, 444]
+    print(search(i, 12))
