88d93e9683e2a2e0f3b96e35b472b83fcfa9a934
chen keyan <676250063@qq.com>
2017-07-21 00:04:55 +0800

    7.21

--- 字符串查找.py
+++ 字符串查找.py
@@ -1,13 +1,22 @@
+# class Solution:
+#     def strStr(self, source, target):
+
+#         if target == '':
+#             return 0
+#             # write your code here
+#         elif target not in source or source == None:
+#             return -1
+
+#         else:
+#             for i in range(0, len(source)):
+#                 if source[i:i + len(target)] == target:
+#                     return i
 class Solution:
     def strStr(self, source, target):
-
-        if target == '':
-            return 0
-            # write your code here
-        elif target not in source or source == None:
+        
+        # write your code here
+        if source == None or target == None or target not in source  :
             return -1
 
         else:
-            for i in range(0, len(source)):
-                if source[i:i + len(target)] == target:
-                    return i
+            return source.find(target)
